de8b56827d7960a41d2252fa3a47006f0047407c
Asutosh <asutosh2001@gmail.com>
2025-05-17 00:35:06 +0530

    Basic TCP server with file upload support
    - Added TCP server on port 8080 using goroutines for concurrency
    - Implemented custom protocol for reading filename and filesize headers
    - Used bufio and io.ReadFull to read file content accurately
    - Currently prints file content; saving to disk to be added next

--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"net"
+	"strconv"
+	"strings"
 )
 
 func main() {
 
-	listener, err := net.Listen("tcp", ":8080")
+	listener, err := net.Listen("tcp", "localhost:8080")
 
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println("Serving on PORT 8080")
 	defer listener.Close()
 
 	for {
@@ -22,28 +27,94 @@ func main() {
 			continue
 		}
 
-		go handleConn(conn)
+		go func(c net.Conn) {
+			defer c.Close()
+			fmt.Println("Client connected: ", c.RemoteAddr())
+			if handleConn(c) {
+				_, err = c.Write([]byte("STATUS: OK\nMESSAGE: File received successfully\n"))
+
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+			} else {
+				_, err = c.Write([]byte("STATUS: NOT OK\nMESSAGE: Error in receiving file\n"))
+
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+			}
+		}(conn)
 	}
 
 }
 
-func handleConn(conn net.Conn) {
-	defer conn.Close()
-	buf := make([]byte, 1024)
+func handleConn(conn net.Conn) bool {
 
+	reader := bufio.NewReader(conn)
+	filename := ""
+	var filesize int = 0
+
+	_ = filename
+	_ = filesize
+
+	readFileContent := false
+
+	fmt.Println("START Reading incoming message...")
 	for {
 
-		n, err := conn.Read(buf)
-
-		if err == nil {
-			fmt.Println(string(buf[:n]))
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error in reading message:", err.Error())
+			return false
 		}
 
-		_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Length: 5\r\n\r\nHello"))
+		line = strings.TrimSpace(line)
 
-		if err != nil {
-			fmt.Println(err)
-			return
+		if line == "" && !readFileContent {
+			readFileContent = true
+			break
+		}
+
+		if !readFileContent {
+			parts := strings.SplitN(line, ":", 2)
+
+			if len(parts) != 2 {
+				fmt.Println("Malformed Headers")
+				return false
+			}
+
+			header := strings.TrimSpace(strings.ToLower(parts[0]))
+			value := strings.TrimSpace(parts[1])
+
+			switch header {
+			case "filename":
+				filename = value
+
+			case "filesize":
+				filesize, err = strconv.Atoi(value)
+				if err != nil {
+					fmt.Println("Invalid filesize:", err)
+					return false
+				}
+			}
+		} else {
+			break
 		}
 	}
+
+	fmt.Printf("START Reading file content from %v of size %v\n", filename, filesize)
+
+	// code for reading file content
+	buf := make([]byte, filesize)
+	n, err := io.ReadFull(reader, buf)
+	if err != nil {
+		fmt.Println("Error in receiving file")
+		return false
+	}
+
+	fmt.Println("File content: ", string(buf[:n]))
+
+	return true
 }
